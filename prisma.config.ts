02646be68e6ef75da602dffd421c03f486588dc8
akshay pawar <ap3690700@gmail.com>
2026-05-01 20:51:13 +0530

    fix: prisma env type issue

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,18 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const dbUrl = process.env.DATABASE_URL;
+
+if (!dbUrl) {
+  throw new Error("DATABASE_URL is not set");
+}
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: dbUrl,
   },
 });
